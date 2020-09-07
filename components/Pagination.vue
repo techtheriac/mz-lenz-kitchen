ea8d01de8bf57095e9aa47f9b35b5fa71ea063d8
techtheriac <mavyjayann@gmail.com>
2020-09-07 10:31:22 +0100

    pagination styles updated

---
+++ components/Pagination.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center">
+    <v-pagination v-model="page" :length="9" :total-visible="5"></v-pagination>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.text-center .v-pagination__item,
+.v-pagination__navigation {
+  box-shadow: none !important;
+}
+</style>
